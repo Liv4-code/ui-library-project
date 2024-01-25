98066b2586d000dab1753e489145b844887186c2
livi <scholtz.ice@gmail.com>
2024-01-25 20:47:55 +0200

    Created dropdown module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-// imports
 import Tooltip from "./ui-config/tooltip";
+import Dropdown from "./ui-config/dropdown";
 
 // create tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 
 tooltip.init();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+    const instance = new Dropdown(dropdown);
+    instance.init();
+});
