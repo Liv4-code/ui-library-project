50a98f8912f8c8ce48449c5ee0df22b194f73811
livi <scholtz.ice@gmail.com>
2024-01-25 21:05:38 +0200

    Created tabs module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Tooltip from "./ui-config/tooltip";
 import Dropdown from "./ui-config/dropdown";
+import Tabs from "./ui-config/tabs";
 
 // create tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
@@ -13,3 +14,7 @@ dropdowns.forEach((dropdown) => {
     const instance = new Dropdown(dropdown);
     instance.init();
 });
+
+// create tabs
+const tabs = new Tabs(document.querySelector(".tabs"));
+tabs.init();
